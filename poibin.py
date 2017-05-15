b011d0e43153bc2f6e8093b747c7063931f3cc46
tsakim <mika.straka@imtlucca.it>
2017-05-15 08:57:37 +0100

    Variable names improved; Docstring to class added.

--- poibin.py
+++ poibin.py
@@ -9,8 +9,9 @@ Author:
     Mika Straka
 
 Description:
-    Implementation of the Poisson Binomial distribution as described in the
-    reference [Hong2013]_.
+    Implementation of the Poisson Binomial distribution for the sum of
+    independent and not identically distributed random variables as described
+    in the reference [Hong2013]_.
 
     Implemented method:
 
@@ -28,17 +29,17 @@ Usage:
         >>> pb = PoiBin(p)
 
     Be ``x`` a list or NumPy array of different number of successes.
-    To obtain:
+    To obtain the:
 
     * probability mass function of x, use::
 
         >>> pb.pmf(x)
 
-    * cumulative distribution function of x, use
+    * cumulative distribution function of x, use::
 
         >>> pb.cdf(x)
 
-    * p-values of x, use
+    * p-values of x, use::
 
         >>> pb.pval(x)
 
@@ -57,13 +58,30 @@ import collections
 import numpy as np
 
 
-class PoiBin:
+class PoiBin(object):
+    """Poisson Binomial distribution for random variables.
 
-    def __init__(self, p):
-        self.p = np.array(p)
-        self.n = self.p.size
+    This class implements the Poisson Binomial distribution for Bernoulli
+    trials with different success probabilities. The distribution describes
+    thus a random variable that is the sum of independent and not identically
+    distributed single Bernoulli random variables.
+
+    The class offers methods for calculating the probability mass function, the
+    cumulative distribution function, and p-values for right-sided testing.
+    """
+
+    def __init__(self, probabilities):
+        """Initialize the class and calculate the ``pmf`` and ``cdf``.
+
+        :param probabilities: sequence of success probabilities :math:`p_i \\in
+            [0, 1] \\forall i \\in [0, N]` for :math:`N` independent but not
+            identically distributed Bernoulli random variables
+        :type probabilities: numpy.array
+        """
+        self.success_probabilities = np.array(probabilities)
+        self.number_trials = self.success_probabilities.size
         self.check_input_prob()
-        self.omega = 2 * np.pi / (self.n + 1)
+        self.omega = 2 * np.pi / (self.number_trials + 1)
         self.pmf_list = self.get_pmf_xi()
         self.cdf_list = self.get_cdf(self.pmf_list)
 
@@ -71,7 +89,7 @@ class PoiBin:
 # Methods for the Poisson Binomial Distribution
 # ------------------------------------------------------------------------------
 
-    def pmf(self, kk):
+    def pmf(self, number_successes):
         """Calculate the probability mass function ``pmf`` for the input values.
 
         The ``pmf`` is defined as
@@ -80,35 +98,38 @@ class PoiBin:
 
             pmf(k) = Pr(X = k), k = 0, 1, ..., n.
 
-        :param kk: integers for which the pmf is calculated
-        :type kk: int or list of ints
+        :param number_successes: number of successful trials for which the
+            probability mass function is calculated
+        :type number_successes: int or list of integers
         """
-        self.check_rv_input(kk)
-        return self.pmf_list[kk]
+        self.check_rv_input(number_successes)
+        return self.pmf_list[number_successes]
 
-    def cdf(self, kk):
+    def cdf(self, number_successes):
         """Calculate the cumulative distribution function for the input values.
 
-        The cumulative distribution function ``cdf`` is defined as
+        The cumulative distribution function ``cdf`` for a number ``k`` of
+        successes is defined as
 
         .. math::
 
-            cdf(k) = Pr(X \leq k), k = 0, 1, ..., n.
+            cdf(k) = Pr(X \\leq k), k = 0, 1, ..., n.
 
-        :param kk: integers for which the cdf is calculated.
-        :type kk: int or list of integers
+        :param number_successes: number of successful trials for which the
+            cumulative distribution function is calculated
+        :type number_successes: int or list of integers
         """
-        self.check_rv_input(kk)
-        return self.cdf_list[kk]
+        self.check_rv_input(number_successes)
+        return self.cdf_list[number_successes]
 
-    def pval(self, kk):
-        """Return the p-value corresponding to the input values kk,
+    def pval(self, number_successes):
+        """Return the p-values corresponding to the input numbers of successes.
 
-        The p-values of right-sided testing are defined as
+        The p-values for right-sided testing are defined as
 
         .. math::
 
-            pval(k) = Pr(X \geq k ),  k = 0, 1, ..., n.
+            pval(k) = Pr(X \\geq k ),  k = 0, 1, ..., n.
 
         .. note::
 
@@ -121,56 +142,63 @@ class PoiBin:
 
                                k = 0, 1, .., n.
 
-        :param kk: integers for which the cdf is calculated.
-        :type kk: int, or np.array or list of integers
+        :param number_successes: number of successful trials for which the
+            p-value is calculated
+        :type number_successes: int, numpy.array, or list of integers
         """
-        self.check_rv_input(kk)
+        self.check_rv_input(number_successes)
         i = 0
         try:
-            isinstance(kk, collections.Iterable)
-            fk = np.array(kk, dtype='float')
+            isinstance(number_successes, collections.Iterable)
+            pvalues = np.array(number_successes, dtype='float')
             # if input is iterable (list, numpy.array):
-            for k in kk:
-                fk[i] = 1. - self.cdf(k) + self.pmf(k)
+            for k in number_successes:
+                pvalues[i] = 1. - self.cdf(k) + self.pmf(k)
                 i += 1
-            return fk
+            return pvalues
         except TypeError:
             # if input is an integer:
-            if kk == 0:
+            if number_successes == 0:
                 return 1
             else:
-                return 1 - self.cdf(kk - 1)
+                return 1 - self.cdf(number_successes - 1)
 
 # ------------------------------------------------------------------------------
 # Methods to obtain pmf and cdf
 # ------------------------------------------------------------------------------
 
-    def get_cdf(self, xx):
+    def get_cdf(self, event_probabilities):
         """Return the values of the cumulative density function.
 
         Return a list which contains all the values of the cumulative
-        density function for :math:`i = 0, 1, ..., n`j.
+        density function for :math:`i = 0, 1, ..., n`.
+
+        :param event_probabilities: array of single event probabilities
+        :type event_probabilities: numpy.array
         """
-        c = np.empty(self.n + 1)
-        c[0] = xx[0]
-        for i in range(1, self.n + 1):
-            c[i] = c[i - 1] + xx[i]
-        return c
+        cdf = np.empty(self.number_trials + 1)
+        cdf[0] = event_probabilities[0]
+        for i in range(1, self.number_trials + 1):
+            cdf[i] = cdf[i - 1] + event_probabilities[i]
+        return cdf
 
     def get_pmf_xi(self):
-        """Return the values of ``xi``.
+        """Return the values of the variable ``xi``.
 
-        The components ``xi`` make up the probability mass function.
+        The components ``xi`` make up the probability mass function, i.e.
+        :math:`\\xi(k) = pmf(k) = Pr(X = k)`.
         """
-        chi = np.empty(self.n + 1, dtype=complex)
+        chi = np.empty(self.number_trials + 1, dtype=complex)
         chi[0] = 1
-        half_n = int(self.n / 2 + self.n % 2)
+        half_number_trials = int(
+            self.number_trials / 2 + self.number_trials % 2)
         # set first half of chis:
-        chi[1:half_n + 1] = self.get_chi(np.arange(1, half_n + 1))
+        chi[1:half_number_trials + 1] = self.get_chi(
+            np.arange(1, half_number_trials + 1))
         # set second half of chis:
-        chi[half_n + 1:self.n + 1] = np.conjugate(chi[1:self.n - half_n + 1]
-                                                  [::-1])
-        chi /= self.n + 1
+        chi[half_number_trials + 1:self.number_trials + 1] = np.conjugate(
+            chi[1:self.number_trials - half_number_trials + 1] [::-1])
+        chi /= self.number_trials + 1
         xi = np.fft.fft(chi)
         if self.check_xi_are_real(xi):
             xi = xi.real
@@ -179,63 +207,77 @@ class PoiBin:
         return xi
 
     def get_chi(self, idx_array):
-        """Return the values of ``chi`` for the specified indices."""
+        """Return the values of ``chi`` for the specified indices.
+
+        :param idx_array: array of indices for which the ``chi`` values should
+            be calculated 
+        :type idx_array: numpy.array
+        """
         # get_z:
-        e = np.exp(self.omega * idx_array * 1j)
-        xy = 1 - self.p + self.p * e[:, np.newaxis]
-        # get__argz_sum:
+        exp_value = np.exp(self.omega * idx_array * 1j)
+        xy = 1 - self.success_probabilities + \
+            self.success_probabilities * exp_value[:, np.newaxis]
+        # sum over the principal values of the arguments of z:
         argz_sum = np.arctan2(xy.imag, xy.real).sum(axis=1)
-        # get_d:
+        # get d value:
         exparg = np.log(np.abs(xy)).sum(axis=1)
-        d = np.exp(exparg)
-        # get_chi:
-        chi = d * np.exp(argz_sum * 1j)
+        d_value = np.exp(exparg)
+        # get chi values:
+        chi = d_value * np.exp(argz_sum * 1j)
         return chi
 
 # ------------------------------------------------------------------------------
 # Auxiliary functions
 # ------------------------------------------------------------------------------
 
-    def check_rv_input(self, kk):
-        """Assert that the input values ``kk`` are OK.
+    def check_rv_input(self, number_successes):
+        """Assert that the input values ``number_successes`` are OK.
 
-        Check that the input values ``kk`` for the random variable are >=0,
-        integers and <= n.
-        """
+        The input values ``number_successes`` for the random variable have to be
+        integers, greater or equal to 0, and smaller or equal to the total
+        number of trials ``self.number_trials``.
+
+        :param number_successes: number of successful trials
+        :type number_successes: int or list of integers """
         try:
-            for k in kk:
+            for k in number_successes:
                 assert (type(k) == int or type(k) == np.int64), \
                         "Values in input list must be integers"
                 assert k >= 0, 'Values in input list cannot be negative.'
-                assert k <= self.n, \
+                assert k <= self.number_trials, \
                     'Values in input list must be smaller or equal to the ' \
                     'number of input probabilities "n"'
         except TypeError:
-            assert (type(kk) == int or type(kk) == np.int64), \
+            assert (type(number_successes) == int or \
+                type(number_successes) == np.int64), \
                 'Input value must be an integer.'
-            assert kk >= 0, "Input value cannot be negative."
-            assert kk <= self.n, \
-                'Input value cannot be greater than ' + str(self.n)
+            assert number_successes >= 0, "Input value cannot be negative."
+            assert number_successes <= self.number_trials, \
+                'Input value cannot be greater than ' + str(self.number_trials)
         return True
 
     @staticmethod
-    def check_xi_are_real(xx):
-        """Check whether all the xis have imaginary part equal to 0.
+    def check_xi_are_real(xi_values):
+        """Check whether all the ``xi``s have imaginary part equal to 0.
 
-        The probabilities ``pmf`` have to be positive.
+        The probabilities :math:`\\xi(k) = pmf(k) = Pr(X = k)` have to be
+        positive and must have imaginary part equal to zero.
+
+        :param xi_values: single event probabilities
+        :type xi_values: complex
         """
         eps = 1e-15  # account for machine precision
-        return np.all(xx.imag <= eps)
+        return np.all(xi_values.imag <= eps)
 
     def check_input_prob(self):
         """Check that all the input probabilities are in the interval [0, 1]."""
-        if self.p.shape != (self.n,):
+        if self.success_probabilities.shape != (self.number_trials,):
             raise ValueError(
                 "Input must be an one-dimensional array or a list.")
-        if not np.all(self.p >= 0):
-            raise ValueError("Input probabilites have to be non negative.")
-        if not np.all(self.p <= 1):
-            raise ValueError("Input probabilites have to be smaller than 1.")
+        if not np.all(self.success_probabilities >= 0):
+            raise ValueError("Input probabilities have to be non negative.")
+        if not np.all(self.success_probabilities <= 1):
+            raise ValueError("Input probabilities have to be smaller than 1.")
 
 ################################################################################
 # Main
